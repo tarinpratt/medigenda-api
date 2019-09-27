d5f8a1ca58586873dc3885ecc9146605bf0b4341
Tarin Pratt <tarinpratt@gmail.com>
2019-09-27 14:01:43 -0700

    changed db names

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/medigenda',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/medigenda',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/medigenda-test',
     JWT_SECRET: process.env.JWT_SECRET || 'casanova',
   }
